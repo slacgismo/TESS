56cf4afe8cbc93d1e3d9ff190806e93c069d71f8
David P. Chassin <dchassin@slac.stanford.edu>
2020-01-14 09:52:32 -0800

    Remove "test" schema support

--- mysql/create_users.sql
+++ mysql/create_users.sql
@@ -12,18 +12,11 @@ CREATE USER 'tess_a'@'localhost' IDENTIFIED BY 'slacgismo';
 GRANT ALL ON `tess`.* TO 'tess_a'@'localhost';
 REVOKE DROP,DELETE ON `tess`.* FROM 'tess_a'@'localhost';
 
-# admin user access to test database
-GRANT ALL ON `test`.* TO 'tess_a'@'localhost';
-REVOKE DELETE ON `test`.* FROM 'tess_a'@'localhost';
-
 # regular user
 CREATE USER 'tess'@'localhost' IDENTIFIED BY 'slacgismo';
 
 # regular user access to TESS database
 GRANT SELECT,UPDATE,INSERT ON `tess`.* TO 'tess'@'localhost';
-
-# regular user access to test database
-GRANT SELECT,UPDATE,INSERT ON `test`.* TO 'tess'@'localhost';
 
 # all done
 FLUSH PRIVILEGES;
