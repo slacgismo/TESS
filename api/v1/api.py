8fea38437c3295995a6af9a0bb493cbf1d242c54
Anna Peery <avpeery@gmail.com>
2020-03-30 09:34:51 -0700

    work in progress: hardcoding json object

--- api/v1/api.py
+++ api/v1/api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/api/v1/meter/meta', methods=['GET'])
 def get_meter_schema():
-    """Returns meter schema as json object"""
+    '''Returns meter schema as json object'''
     
     schema_data = []
  
@@ -21,3 +21,19 @@ def get_meter_schema():
                             'example': row.example})
 
     return jsonify(schema_data)
+
+    # alternate with hardcode:
+    # schema_data = [{'attribute': 'uid', 
+    #                 'format': 'uid string', 
+    #                 'description': 'unique id for meter'},
+    #                {'attribute': 'utility_uid',
+    #                 'format': 'uid string',
+    #                 'description': 'unique id for utility to which the meter belongs',
+    #                 'example': 'xyz123p'},
+    #                 {'attribute': 'authorization_uid',
+    #                  'format': 'uid string',
+    #                  'description': 'the authorization under which meter belongs',
+    #                  'attribute': 'uid strin}
+    #           ]
+
+    
