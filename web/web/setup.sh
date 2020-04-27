fec95f34ba0473bf2a6ebe5c17f22f275626459f
David P. Chassin <dchassin@slac.stanford.edu>
2020-04-27 16:16:46 -0700

    Remove mysql root password change on app side

--- web/web/setup.sh
+++ web/web/setup.sh
@@ -4,9 +4,5 @@ conda create -n venv_tess python=3.8
 conda activate venv_tess
 pip install -r requirements.txt
 
-PASSWORD=$(openssl rand -base64 20 | tr -cd 'A-Za-z0-9')
-mysqladmin -u root root "${PASSWORD:-root}"
-echo '*** 'MYSQL ROOT PASSWORD is ${PASSWORD:-root}' ***'
-
 export FLASK_APP=web
 flask run
