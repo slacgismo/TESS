cb4a863c5250299f6faf2239f230e89ded8d6078
David P. Chassin <dchassin@slac.stanford.edu>
2020-04-28 08:35:06 -0700

    Update setup.sh

--- web/web/setup.sh
+++ web/web/setup.sh
@@ -4,5 +4,10 @@ conda create -n venv_tess python=3.8
 conda activate venv_tess
 pip install -r requirements.txt
 
+yum install net-tools -y
+IFCONF=$(ifconfig eth0 | grep inet)
+IPADDR=$(echo $IFCONF | cut -f2 -d' ')
+echo "To access this server, use http://$IPADDR:5000/"
+
 export FLASK_APP=/web
 flask run
