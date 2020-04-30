62e436f81f5424708ef1a02671b012a3f505bb92
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-04-30 16:16:14 -0700

    some more chart update

--- web/web/cost_revenue/static/cost_revenue.js
+++ web/web/cost_revenue/static/cost_revenue.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
+import IntegralChart from './integral_chart';
 import CashFlowChart from './cash_flow_chart';
+import SurplusWelfareChart from './surplus_welfare_chart';
 import ConnectedComponentWrapper from '../../static/js/base';
 
 import * as action from './actions';
@@ -14,10 +16,18 @@ class CostRevenue extends React.Component {
             <React.Fragment>
                 <div className="chart-row-container">
                     <div className="chart-container">
-                        <CashFlowChart xTitle="Time" yTitle="$/h"/>
+                        <CashFlowChart xTitle="Time" yTitle="$/h" chartTitle="Cash Flow" />
                     </div>
                     <div className="chart-container">
-                        <CashFlowChart xTitle="∫ Qdt" yTitle="$" />
+                        <CashFlowChart xTitle="∫ Qdt" yTitle="$" chartTitle="Cash Flow" />
+                    </div>
+                </div>
+                <div className="chart-row-container">
+                    <div className="chart-container">
+                        <SurplusWelfareChart xTitle="Time" yTitle="$/h" chartTitle="Surplus / Welfare"/>
+                    </div>
+                    <div className="chart-container">
+                        <IntegralChart xTitle="Time" yTitle="$" chartTitle="Integral w.r.t Target" />
                     </div>
                 </div>
             </React.Fragment>
