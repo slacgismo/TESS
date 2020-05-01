76a353e6e2b93aa4b585c89fe7de09ca9ccb8fa7
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-05-01 10:24:35 -0700

    Rework some of the charting

--- web/web/cost_revenue/static/cost_revenue.js
+++ web/web/cost_revenue/static/cost_revenue.js
@@ -1,36 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
-import IntegralChart from './integral_chart';
-import CashFlowChart from './cash_flow_chart';
-import SurplusWelfareChart from './surplus_welfare_chart';
+import CostRevenueChart from './cost_revenue_charts';
 import ConnectedComponentWrapper from '../../static/js/base';
 
 import * as action from './actions';
 
 class CostRevenue extends React.Component {
-    onClick = () => {}
-
     render() {
         return (
-            <React.Fragment>
+            <div className="cost-revenue-main-content-container">
                 <div className="chart-row-container">
-                    <div className="chart-container">
-                        <CashFlowChart xTitle="Time" yTitle="$/h" chartTitle="Cash Flow" />
+                    <div className="chart-container">                        
+                        <CostRevenueChart 
+                            xTitle="Time" 
+                            yTitle="$/h" 
+                            chartTitle="Cash Flow"
+                            chartSubtitle="" />
                     </div>
+                    <div className="chart-container chart-divider"></div>
                     <div className="chart-container">
-                        <CashFlowChart xTitle="∫ Qdt" yTitle="$" chartTitle="Cash Flow" />
+                        <CostRevenueChart 
+                            xTitle="Time" 
+                            yTitle="$/h" 
+                            chartTitle="Surplus / Welfare"
+                            chartSubtitle="" />
                     </div>
                 </div>
+                <br />
                 <div className="chart-row-container">
                     <div className="chart-container">
-                        <SurplusWelfareChart xTitle="Time" yTitle="$/h" chartTitle="Surplus / Welfare"/>
+                        <CostRevenueChart 
+                            xTitle="∫ Qdt" 
+                            yTitle="$"
+                            chartTitle="Cash Flow"
+                            chartSubtitle="" />                        
                     </div>
+                    <div className="chart-container chart-divider"></div>
                     <div className="chart-container">
-                        <IntegralChart xTitle="Time" yTitle="$" chartTitle="Integral w.r.t Target" />
+                        <CostRevenueChart 
+                            xTitle="Time" 
+                            yTitle="$" 
+                            chartTitle="Integral W.R.T. Target"
+                            chartSubtitle="" />
                     </div>
                 </div>
-            </React.Fragment>
+            </div>
         );
     }
 }
