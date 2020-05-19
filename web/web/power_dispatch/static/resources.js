c2188f4bfeb9848485ce0564b8fdee9db49871b0
jongoncalves <jonathan.goncalves@gmail.com>
2020-05-19 07:22:52 -0700

    Remove the react google chart lib

--- web/web/power_dispatch/static/resources.js
+++ web/web/power_dispatch/static/resources.js
@@ -1,34 +1,8 @@
 import React from "react";
-import { Chart } from "react-google-charts";
 
 const ResourcesChart = props => {
     return (
-        <Chart
-            width={'500px'}
-            height={'300px'}
-            chartType="Bar"
-            loader={<div>Loading Chart</div>}
-            data={[
-                ['', 'Unvailable', 'Available', 'Dispatched'],
-                ['Total', 1000, 400, 200],
-                ['Batteries', 1170, 460, 250],
-                ['Chargers', 1660, 120, 300],
-                ['PV', 1030, 540, 350],
-                ['HVAC', 1030, 540, 350],
-                ['Hot Water', 1030, 540, 350],
-            ]}
-            options={{
-                // Material design options
-                chart: {
-                    title: props.chartTitle,
-                    subtitle: '',
-                },
-                isStacked: 'percent',
-                bars: 'horizontal'
-            }}
-            // For tests
-            rootProps={{ 'data-testid': '2' }}
-        />
+        null
     )
 };
 
