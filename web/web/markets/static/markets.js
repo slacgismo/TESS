8ee6ca12841e70da806d6e60daabacc27b8d9069
Jonathan Goncalves <jongoncalves@users.noreply.github.com>
2020-05-20 08:39:06 -0700

    Frontend markets (#50)
    * New 404 image
    
    * finished up initial pass on markets page

--- web/web/markets/static/markets.js
+++ web/web/markets/static/markets.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as action from './actions';
+import AuctionChart from './auction';
 import { connect } from 'react-redux';
+import { Switch } from '@rmwc/switch';
+import { Button } from '@rmwc/button';
+import HistoricalChart from './historical';
+import { TextField } from '@rmwc/textfield';
+import AuctionMarketChart from './auction_market';
 import { selectMenuOption } from '../../static/js/actions';
 import ConnectedComponentWrapper from '../../static/js/base';
 
-import * as action from './actions';
+import '@rmwc/switch/styles';
+import '@rmwc/button/styles';
+import '@rmwc/textfield/styles';
 
 class Markets extends React.Component {
     componentDidMount() {
@@ -17,7 +26,86 @@ class Markets extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div className="power-dispatch-container">
+                <div className="power-dispatch-margin-fix">
+                    <div className="power-dispatch-chart-container">
+                        <div className="pd-chart-system-load">
+                            <AuctionMarketChart
+                                id="auction-market-chart"
+                                xTitle="Capacity (MW)" 
+                                yTitle="Price ($/MWh)"
+                                chartTitle="Auction Market"
+                                chartSubtitle="Transformer Capacity" />
+                        </div>
+                        <div className="pd-chart-resource">
+                            <AuctionChart
+                                id="auction-chart"
+                                xTitle="Time" 
+                                yTitle="Price ($/MWh)"
+                                chartTitle="Auction"
+                                chartSubtitle="" />
+                        </div>
+                    </div>
+
+                    <div className="power-dispatch-forms-container">
+                        <div className="pd-form-container">
+                            <div className="pd-form-title">
+                                <h3>Constraint and Alert Settings</h3>
+                            </div>
+
+                            <div>
+                                <h4>Alarms</h4>
+                                <div className="pd-form-row">
+                                    <div className="pd-form-element-label">Yellow Alarm</div>
+                                    <div className="pd-form-element-input">
+                                        <TextField outlined />
+                                    </div>
+                                    <div className="pd-form-element-unit">%</div>
+                                </div>
+                                <div className="pd-form-row">
+                                    <div className="pd-form-element-label">Red Alarm</div>
+                                    <div className="pd-form-element-input">
+                                        <TextField outlined />
+                                    </div>
+                                    <div className="pd-form-element-unit">%</div>
+                                </div>
+                            </div>
+                            
+                            <hr />
+                            
+                            <div>
+                                <h4>Alerts</h4>
+                                <div className="pd-form-row">
+                                    <div className="pd-form-element-label">Price</div>
+                                    <div className="pd-form-element-input">
+                                        <TextField outlined />
+                                    </div>
+                                    <div className="pd-form-element-unit">Percentile</div>
+                                </div>
+                            </div>
+                            
+                            <hr />
+                            
+                            <div className="pd-form-button-container">
+                                <Button 
+                                    label="SET" 
+                                    onClick={this.addNewRow}
+                                    outlined />
+                            </div>
+                        </div>    
+                    
+                        <div className="pd-form-container">
+                            <div className="pd-chart-system-load">
+                                <HistoricalChart
+                                    id="historical-market-chart"
+                                    xTitle="Time" 
+                                    yTitle="Price ($/MWh)"
+                                    chartTitle="Historical Data" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
