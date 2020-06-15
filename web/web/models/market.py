9a871dce6265470c30eea8af1981f2c48fcb3a9a
Anna Peery <avpeery@gmail.com>
2020-06-15 15:46:54 -0700

    Market Interval and Market models added with corresponding columns to ERD and data dictionary - WIP: adding default values on market interval

--- web/web/models/market.py
+++ web/web/models/market.py
@@ -1,12 +1,5 @@
 from datetime import datetime, timedelta
 from sqlalchemy.types import TIMESTAMP
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow import fields, ValidationError
-
-from web.models.utility import Utility
-from web.models.channel import Channel
-from web.models.interval import Interval
-from web.models.service_location import ServiceLocation, ServiceLocationSchema
 from web.database import (
     db,
     Model,
@@ -16,34 +9,38 @@ from web.database import (
     reference_col,
 )
 
-# class Market(Model):
-#     __tablename__ = 'markets'
+class Market(Model):
+    __tablename__ = 'markets'
 
-#     market_id = Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-#     utility_id = Column(db.Integer,
-#                         db.ForeignKey('utilities.utility_id'),
-#                         primary_key=True,
-#                         nullable=False)
-#     service_location_id = Column(
-#         db.String(64),
-#         db.ForeignKey('service_locations.service_location_id'),
-#         primary_key=True,
-#         nullable=False)
+    market_id = Column(db.Integer, 
+                       primary_key=True, 
+                       autoincrement=True, 
+                       nullable=False)
 
-#     feeder = Column(db.String(45), nullable=False)
-#     substation = Column(db.String(45), nullable=False)
-#     meter_type = Column(db.Enum(MeterType), nullable=False)
-#     is_active = Column(db.Boolean(False), nullable=False)
-#     is_archived = Column(db.Boolean(False), nullable=False)
-#     created_at = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
-#     updated_at = Column(TIMESTAMP,
-#                         nullable=False,
-#                         default=datetime.utcnow,
-#                         onupdate=datetime.utcnow)
+    source = Column(db.Text,
+                    nullable=False)
 
-#     # Relationships
-#     service_location = relationship('ServiceLocation',
-#                                     backref=db.backref('meters'))
-#     utility = relationship('Utility', backref=db.backref('meters'))
-#     channels = relationship('Channel', backref=db.backref('meters'))
-#     intervals = relationship('Interval', backref=db.backref('meters'))
+    ts = Column(db.Integer,
+                nullable=False)
+
+    p_max=Column(db.Float,
+                 nullable=False)
+    
+    is_active=Column(db.Boolean,
+                     default=False,
+                     nullable=False)
+
+    is_archived=Column(db.Boolean,
+                       default=False,
+                       nullable=False)
+
+    created_at = Column(TIMESTAMP, 
+                        default=datetime.utcnow,
+                        nullable=False)
+    updated_at = Column(TIMESTAMP,
+                        default=datetime.utcnow,
+                        onupdate=datetime.utcnow,
+                        nullable=False)
+
+    def __repr__(self):
+        return f'<Market market_id={self.market_id} p_max={self.p_max} created_at={self.created_at}>'
---
+++ web/web/models/market_interval.py
@@ -0,0 +1,49 @@
+from sqlalchemy.types import TIMESTAMP
+from web.database import (
+    db,
+    Model,
+    Column,
+    SurrogatePK,
+    relationship,
+    reference_col,
+)
+
+class MarketInterval(Model):
+    __tablename__ = 'market_intervals'
+    
+    market_interval_id = Column(db.Integer,
+                                primary_key=True,
+                                autoincrement=True,
+                                nullable=False)
+
+    market_id = Column(db.Integer, 
+                       db.ForeignKey('markets.market_id'),
+                       nullable=False)
+
+    p_exp = Column(db.Float,
+                   nullable=False)
+    
+    p_dev = Column(db.Float,
+                   nullable=False)
+    
+    p_clear = Column(db.Float,
+                   nullable=False)
+    
+    q_clear = Column(db.Float,
+                     nullable=False)
+
+    alpha = Column(db.Float,
+                   nullable=False)
+
+    start_time = Column(TIMESTAMP,
+                        nullable=False)
+
+    end_time = Column(TIMESTAMP,
+                      nullable=False)
+    
+    # Relationships
+    market = relationship('Market',
+                          backref=db.backref('market_intervals'))
+
+    def __repr__(self):
+        return f'<MarketInterval market_interval_id={self.market_interval_id} market_id={self.market_id}>'
