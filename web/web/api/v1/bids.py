e2141ca0a7e74cf87f8a72e7365a413f834ec8f9
jongoncalves <jonathan.goncalves@gmail.com>
2020-09-02 20:59:01 -0700

    add the more to the bid api

--- web/web/api/v1/bids.py
+++ web/web/api/v1/bids.py
@@ -1,11 +1,12 @@
 from web.database import db
+import dateutil.parser as parser
 from flask import request, Blueprint
 from marshmallow import ValidationError
 from web.models.hce_bids import HceBids
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 from .response_wrapper import ApiResponseWrapper
 from web.models.meter_interval import MeterInterval
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 bids_api_bp = Blueprint('bids_api_bp', __name__)
 
@@ -13,15 +14,33 @@ bids_api_bp = Blueprint('bids_api_bp', __name__)
 @bids_api_bp.route('/bids/', methods=['GET'])
 def get_bids():
     '''
-    
+    Get the supply or demand bids 
     '''
     arw = ApiResponseWrapper()
+    start_time = request.args.get('start_time', None)
+    is_supply = request.args.get('is_supply', None)
 
     try:
         results = []
+        if start_time:
+            start_time = parser.parse(start_time)
 
-    except (MultipleResultsFound, NoResultFound):
-        arw.add_errors('No result found or multiple results found')
+        if is_supply.lower() == 'false':
+            # get the hce bids only
+
+        if is_supply.lower() == 'true':
+            # get the meter interval and hce bids
+
+        # otherwise, just get both
+
+    except parser.ParserError as pe:
+        arw.add_errors(
+            'Could not parse the date time value. Please provide a valid format.'
+        )
+        return arw.to_json(None, 400)
+
+    except NoResultFound:
+        arw.add_errors('No result found')
 
     if arw.has_errors():
         return arw.to_json(None, 400)
