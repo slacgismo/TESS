06f2593d4ce33b0847e6937819b1f766eb801af0
jongoncalves <jonathan.goncalves@gmail.com>
2020-09-14 21:03:31 -0700

    add eventing devices setup and allow api to serve as webhook to pub sub on aws sns/iot

--- web/web/api/v1/device_event_source.py
+++ web/web/api/v1/device_event_source.py
@@ -4,7 +4,10 @@ from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 from .response_wrapper import ApiResponseWrapper
-from web.models.device_event_source import DeviceEventSource, DeviceEventSourceSchema
+from web.models.device_event_source import (DeviceEventSource,
+                                            DeviceEventSourceSchema,
+                                            EventingDevice,
+                                            EventingDeviceSchema)
 
 des_api_bp = Blueprint('des_api_bp', __name__)
 
@@ -67,5 +70,67 @@ def add_des():
         return arw.to_json(None, 400)
 
     result = DeviceEventSourceSchema().dump(new_des)
+
+    return arw.to_json(result)
+
+
+@des_api_bp.route('/eventing_devices', methods=['GET'])
+def get_evt_des():
+    '''
+    Get EVT DES
+    '''
+    arw = ApiResponseWrapper()
+    des_schema = EventingDeviceSchema()
+    des = EventingDevice.query.all()
+    results = des_schema.dump(des, many=True)
+    return arw.to_json(results)
+
+
+@des_api_bp.route('/eventing_device/<int:des_id>', methods=['GET'])
+def show_evt_des(des_id):
+    '''
+    Returns des
+    '''
+
+    arw = ApiResponseWrapper()
+    des_schema = EventingDeviceSchema()
+
+    try:
+        des = EventingDevice.query.filter_by(des_id=des_id).one()
+    except (MultipleResultsFound, NoResultFound):
+        arw.add_errors('No result found or multiple results found')
+        return arw.to_json(None, 400)
+
+    results = des_schema.dump(des)
+
+    return arw.to_json(results)
+
+
+@des_api_bp.route('/eventing_devices', methods=['POST'])
+def add_evt_des():
+    '''
+    Adds new eventing device entry
+    '''
+
+    arw = ApiResponseWrapper()
+    des_schema = EventingDeviceSchema()
+    des_json = request.get_json()
+
+    try:
+        new_des = des_schema.load(des_json, session=db.session)
+        db.session.add(new_des)
+        db.session.commit()
+
+    except ValidationError as ve:
+        arw.add_errors(ve.messages)
+
+    except IntegrityError:
+        arw.add_errors('Integrity error')
+
+    if arw.has_errors():
+        db.session.rollback()
+        return arw.to_json(None, 400)
+
+    result = EventingDeviceSchema().dump(new_des)
 
     return arw.to_json(result)
--- web/web/models/device_event_source.py
+++ web/web/models/device_event_source.py
@@ -4,6 +4,21 @@ from web.database import (db, Model, Column)
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 
+class EventingDevice(Model):
+    __tablename__ = 'eventing_devices'
+    ed_id = Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True,
+                   nullable=False)
+    device_id = Column(db.String(64), unique=True)
+
+
+class EventingDeviceSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = EventingDevice
+        load_instance = True
+
+
 class DeviceEventSource(Model):
     __tablename__ = 'device_events'
     des_id = Column(db.Integer,
@@ -12,6 +27,10 @@ class DeviceEventSource(Model):
                     nullable=False)
     event_data = Column(db.JSON)
     created_at = Column(TIMESTAMP, server_default=func.now())
+    ed_id = Column(db.Integer,
+                   db.ForeignKey(EventingDevice.ed_id),
+                   unique=False,
+                   nullable=False)
 
 
 class DeviceEventSourceSchema(SQLAlchemyAutoSchema):
