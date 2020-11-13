d46ed2fd82159164a3fb763214b412f31dba99e9
Anna Peery <42715836+avpeery@users.noreply.github.com>
2020-11-13 13:02:08 -0800

    Implement sendgrid (#72)
    * WIP: implementing send grid in place of SMTP
    
    * wip: sendgrid implementation to multiple recepients
    
    * Fixed sending emails with sendgrid
    
    * removed print statements
    
    * removed extra sendgrid.env in gitignore file
    
    * specified error handling

--- web/web/emails/send_emails.py
+++ web/web/emails/send_emails.py
@@ -1,37 +1,34 @@
-import smtplib
-from dotenv import load_dotenv
-load_dotenv()
-
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from python_http_client.exceptions import (BadRequestsError, UnauthorizedError, TooManyRequestsError,
+                                           PayloadTooLargeError, ServiceUnavailableError, InternalServerError,
+                                           UnsupportedMediaTypeError, NotFoundError, ForbiddenError)
 import os
-from email.mime.text import MIMEText
-
-EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
-PASSWORD = os.getenv('PASSWORD')
-
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
 
 
-def send_email(subject, message, bcc):
-    '''Simple SMTP set up with gmail'''
+def send_email(alert_type, notification_message, email_address):
+    '''sendgrid email setup'''
 
-    # Simple set up for basic working email functionality
-    # TODO: styling email, switch to implementing OAuth/Gmail API
+    message = Mail(
+        from_email=os.getenv('EMAIL_ADDRESS'),
+        to_emails=email_address
+    )
+
+    message.dynamic_template_data = {
+        "subject": "TESS Notification: " + alert_type + " alert",
+        "notification_message": notification_message
+    }
+
+    message.template_id = os.environ.get('SENDGRID_NOTIFICATION_TEMPLATE_ID')
 
     try:
-        server = smtplib.SMTP('smtp.gmail.com:587')
 
-        msg = MIMEText(message)
-        msg['From'] = EMAIL_ADDRESS
-        msg['Subject'] = subject
+        sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sendgrid_client.send(message)
+        print(response.status_code)
 
-        server.ehlo()
-        server.starttls()
-        server.login(EMAIL_ADDRESS, PASSWORD)
+    except (BadRequestsError, UnauthorizedError, NotFoundError, PayloadTooLargeError,
+            TooManyRequestsError, UnsupportedMediaTypeError, ServiceUnavailableError,
+            InternalServerError, ForbiddenError) as e:
 
-        server.sendmail(EMAIL_ADDRESS, bcc, msg.as_string())
-        server.quit()
-
-    except:
-        print('email failed to send')
+        print(e.body)
