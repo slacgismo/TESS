e92125585ce66bced558001f406392697486a7e8
Derin Serbetcioglu <serbetd@gmail.com>
2020-11-23 19:41:22 -0800

    removed unused lines

--- web/web/api/v1/sns_read.py
+++ web/web/api/v1/sns_read.py
@@ -1,23 +1,8 @@
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from .response_wrapper import ApiResponseWrapper
 from flask import Flask, request, Blueprint
-from sqlalchemy.exc import IntegrityError
-from marshmallow import ValidationError
-from web.database import db
 import requests
 import json
 
-
 sns_read_api_bp =  Blueprint('sns_read_api_bp', __name__)
-
-def parseRealPower(message):
-    ''' converts byte wtring to dict and retrieves a sunnyboy_inverter.calc_ac_power_kw
-        data and timestamp as a dict
-    '''
-    payload_str = message.payload.decode("UTF-8")
-    payload = ast.literal_eval(payload_str)
-    power_data = payload["DeviceInformation"][0]["sunnyboy_inverter.calc_ac_power_kw"]
-    return power_data
 
 
 @sns_read_api_bp.route('/sns_read', methods = ['GET', 'POST'])
@@ -35,3 +20,13 @@ def sns():
     if hdr == 'Notification':
         parseRealPower(js['Message'])
     return 'OK\n'
+
+
+def parseRealPower(message):
+    ''' converts byte wtring to dict and retrieves a sunnyboy_inverter.calc_ac_power_kw
+        data and timestamp as a dict
+    '''
+    payload_str = message.payload.decode("UTF-8")
+    payload = ast.literal_eval(payload_str)
+    power_data = payload["DeviceInformation"][0]["sunnyboy_inverter.calc_ac_power_kw"]
+    return power_data
