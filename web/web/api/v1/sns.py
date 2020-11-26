83701cf02be336faeb8bac24e4f84fa88142e318
Derin Serbetcioglu <serbetd@gmail.com>
2020-11-25 16:31:51 -0800

    new comments adressed

--- web/web/api/v1/sns.py
+++ web/web/api/v1/sns.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, Blueprint
+import logging
+import requests
 import json
 
 sns_api_bp =  Blueprint('sns_api_bp', __name__)
-
+logging.basicConfig(filename='sns.log', level=logging.INFO)
 
 @sns_api_bp.route('/sns', methods = ['POST'])
 def subscriber_information():
     # AWS sends JSON with text/plain mimetype
     try:
         js = json.loads(request.data)
-    except:
-        return 'An error occuered'
+    except ValueError as err:
+        logging.error(err)
     hdr = request.headers.get('x-amz-sns-message-type')
     # subscribe to the SNS topic
     if hdr == 'Notification':
         parse_real_power(js['Message'])
     elif hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
-        return js['SubscribeURL']
-    return 'OK'
-
+        r = requests.get(js['SubscribeURL'])
+    logging.info('Success')
+    return "OK\n"
 
 def parse_real_power(message):
     ''' converts byte wtring to dict and retrieves a sunnyboy_inverter.calc_ac_power_kw
