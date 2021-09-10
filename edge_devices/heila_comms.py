42d2e7ff143eea99f5bea447db0f5fa9ef195217
Derin Serbetcioglu <serbetd@gmail.com>
2021-09-10 09:55:23 -0400

    Edgedevices (#137)
    * added timeout for requests
    
    * messages parsed from heila to align with DB and implementation of market timing
    
    * changing timestamp to string
    
    * updated subscriber method
    
    * updateing clientID
    
    Co-authored-by: gcezar <gcezar@stanford.edu>

--- edge_devices/heila_comms.py
+++ edge_devices/heila_comms.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import Timeout
 import json
 import config
 
@@ -8,10 +9,13 @@ def heila_set_real_power(url, val=10000):
     real_power_endpoint = '/api/set-real-power'
     payload = {'key':'real_power', 'value': str(val)}
     try:
-        retval = requests.post(url+real_power_endpoint, json=payload)
+        retval = requests.post(url+real_power_endpoint, json=payload, timeout=config.TIMEOUT)
     except requests.exceptions.RequestException as e:
         print('Request error: ', e)
         return None
+    except Timeout:
+        print('Heila timeout')
+        return 'Timeout'
 
     return {'status_code': retval.status_code}
 
@@ -19,10 +23,13 @@ def heila_set_real_power(url, val=10000):
 def heila_update(url):
     update = '/api/update'
     try:
-        retval = requests.get(url + update)
+        retval = requests.get(url + update, timeout=config.TIMEOUT)
     except requests.exceptions.RequestException as e:
         print('Request error: ', e)
         return None
+    except Timeout:
+        print('Heila timeout')
+        return 'Timeout'
 
     if retval.status_code == 200:
         return json.loads(retval.text)['readings']
@@ -31,8 +38,8 @@ def heila_update(url):
 
 
 # Testing
-url = config.URL
-retval_update = heila_update(url=url)
-retval_real_power = heila_set_real_power(url=url, val=10000)
+# url = config.URL
+# retval_update = heila_update(url=url)
+# retval_real_power = heila_set_real_power(url=url, val=10000)
 
 
